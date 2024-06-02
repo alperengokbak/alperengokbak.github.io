38cf325216bc10e2f69d3df28f8cd5568bee6b5f
Alperen Gökbak <swalperen3008@gmail.com>
2024-06-03 01:57:43 +0300

    Refactoring: Added hover effect.

--- src/components/SocialMediaComponent.jsx
+++ src/components/SocialMediaComponent.jsx
@@ -9,16 +9,26 @@ import Medium from "../assets/Logo/02_White/Symbol/PNG/RGB/Medium-Symbol-White-R
 export default function SocialMediaLinks() {
   return (
     <div className="flex flex-row justify-center items-center w-full mt-12 xs:gap-6 sm:gap-12 md:gap-12 lg:gap-24">
-      <a href="https://github.com/alperengokbak" target="_blank" rel="noopener noreferrer">
+      <a href="https://github.com/alperengokbak" target="_blank" rel="noopener noreferrer" className="social-media-btn">
         <img src={Github} alt="GitHub" className="md:size-12 xs:size-10" />
       </a>
-      <a href="https://medium.com/@swalperen3008" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://medium.com/@swalperen3008"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-media-btn"
+      >
         <img src={Medium} alt="LinkedIn" className="md:size-12 xs:size-10" />
       </a>
-      <a href="https://www.linkedin.com/in/alperen-gokbak-68988a225/" target="_blank" rel="noopener noreferrer">
+      <a
+        href="https://www.linkedin.com/in/alperen-gokbak-68988a225/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="social-media-btn"
+      >
         <img src={LinkedIn} alt="GitHub" className="md:size-11 xs:size-9" />
       </a>
-      <a href="https://x.com/Alperengokbak" target="_blank" rel="noopener noreferrer">
+      <a href="https://x.com/Alperengokbak" target="_blank" rel="noopener noreferrer" className="social-media-btn">
         <img src={Twitter} alt="LinkedIn" className="md:size-11 xs:size-9" />
       </a>
     </div>
